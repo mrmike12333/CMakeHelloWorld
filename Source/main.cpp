6cf31e9b8cb4c15e7103315d4ad423240ebd58b0
mrmike12333 <32998983+mrmike12333@users.noreply.github.com>
2024-04-06 13:54:37 +0100

    Added config file and function to dump project information

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include "HelloWorldConfig.h"
+
+/**
+ * @brief Output CMAKE config information to the console.
+ * 
+ */
+void drawProjectHeader()
+{
+    std::cout << "======" << std::endl;
+    std::cout << "Project Name: " 
+         << PROJECT_NAME << std::endl;
+    std::cout << "Description: " 
+         << PROJECT_DESCRIPTION << std::endl;
+    std::cout << "Version: " << VERSION_MAJOR << 
+    "." << VERSION_MINOR << std::endl;
+    std::cout << "======" << std::endl;
+}
 
 int main()
 {
+    drawProjectHeader();
+
     // Some arbitrary C++14 code to test make requirement
     auto myInt = std::make_unique<int>(5);
 
