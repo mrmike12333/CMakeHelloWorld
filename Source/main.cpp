879b57524dafb8f44adf305cb48022ad67d9ad59
mrmike12333 <32998983+mrmike12333@users.noreply.github.com>
2024-04-06 13:05:04 +0100

    Added c++ version requirement with C++14 code

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // Some arbitrary C++14 code to test make requirement
+    auto myInt = std::make_unique<int>(5);
+
+    for (int i = 0; i < *myInt; ++i)
+        std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
